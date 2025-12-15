a62f399aa95674745bcb363e4a783345e2dd7ac1
possiblemeatball <meatball@manthrowshat.net>
2025-12-15 02:05:41 -0500

    waitress start/stop and serve hilariously buggy and unimplemented but a bare start indeed!

--- src/dexcom_browser_source/browser_source.py
+++ src/dexcom_browser_source/browser_source.py
@@ -1,41 +1,95 @@
-from typing import override
-from PySide6.QtCore import QThread
-from PySide6.QtWidgets import QDialog, QWidget
+from waitress.server import BaseWSGIServer, MultiSocketServer
+from pydexcom.dexcom import Dexcom
+from PySide6.QtCore import QObject, QThread, Qt
+from PySide6.QtWidgets import QDialog, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget
 from flask import Blueprint, Flask
-from waitress import serve
+from flask.views import ft
+from waitress.server import create_server
 
 from dexcom_browser_source.config import AppConfig
 
 
-class WaitressThread(QThread):
+class WaitressWorker(QObject):
     def __init__(self, app_config: AppConfig):
-        super().__init__()
         self._app_config: AppConfig = app_config
+        self._waitress_server: MultiSocketServer | BaseWSGIServer = create_server(
+            application=create_app(app_config=self._app_config))
+        super().__init__()
 
-    @override
     def run(self, /) -> None:
-        serve(app=create_app(self._app_config))
+        self._waitress_server.run()
 
-class WaitressStatusDialog(QDialog):
-    def __init__(self, waitress_thread: WaitressThread, parent: QWidget | None = None):
-        super().__init__(parent)
-        self._waitress_thread: WaitressThread = waitress_thread
-        self.setWindowTitle("Waitress Status - Dexcom Browser Source")
+    def close(self, /) -> None:
+        self._waitress_server.close()
 
 class BrowserSourceDetailsDialog(QDialog):
-    def __init__(self, waitress_thread: WaitressThread, parent: QWidget | None = None):
+    def __init__(self, app_config: AppConfig, parent: QWidget | None = None):
+        self._app_config: AppConfig = app_config
+        self._waitress_thread: QThread = QThread()
+        self._waitress_worker: WaitressWorker = WaitressWorker(app_config=app_config)
+        self._layout: QVBoxLayout = QVBoxLayout()
+        self._button_layout: QHBoxLayout = QHBoxLayout()
+        self._waitress_status_label: QLabel = QLabel()
+        self._waitress_start_button: QPushButton = QPushButton("Start Waitress")
+        self._waitress_stop_button: QPushButton = QPushButton("Stop Waitress")
         super().__init__(parent)
-        self._waitress_thread: WaitressThread = waitress_thread
         self.setWindowTitle("Browser Source Details - Dexcom Browser Source")
+
+        self._waitress_status_label.setTextFormat(Qt.TextFormat.MarkdownText)
+        self._waitress_status_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self._layout.addWidget(self._waitress_status_label)
+        self._button_layout.addWidget(self._waitress_start_button)
+        self._button_layout.addWidget(self._waitress_stop_button)
+        self._layout.addLayout(self._button_layout)
+        self.setLayout(self._layout)
+
+        self.on_waitress_finish()
+        _ = self._waitress_thread.started.connect(self.on_waitress_start)
+        _ = self._waitress_thread.finished.connect(self.on_waitress_finish)
+        _ = self._waitress_start_button.clicked.connect(self.start_waitress)
+        _ = self._waitress_stop_button.clicked.connect(self.stop_waitress)
+
+    def start_waitress(self):
+        _ = self._waitress_worker.moveToThread(self._waitress_thread)
+        _ = self._waitress_thread.finished.connect(self._waitress_worker.deleteLater)
+        self._waitress_thread.start()
+
+    def stop_waitress(self):
+        self._waitress_thread.exit()
+
+    def on_waitress_start(self):
+        self._waitress_start_button.setText("Restart Waitress")
+        self._waitress_stop_button.setEnabled(True)
+
+        self._waitress_status_label.setText("# Waitress is Online")
+        self._waitress_status_label.setStyleSheet("QLabel { color: green; }")
+
+    def on_waitress_finish(self):
+        self._waitress_thread = QThread()
+        self._waitress_start_button.setText("Start Waitress")
+        self._waitress_stop_button.setEnabled(False)
+
+        self._waitress_status_label.setText("# Waitress is Offline")
+        self._waitress_status_label.setStyleSheet("QLabel { color: red; }")
 
 # flask app for waitress to serve
 def create_app(app_config: AppConfig) -> Flask:
     app: Flask = Flask(__name__)
-    glucose_blueprint: Blueprint = Blueprint('glucose', import_name=__name__, url_prefix='/glucose')
-    glucose_blueprint.add_url_rule(rule='/')
-    chart_blueprint: Blueprint = Blueprint('chart', import_name=__name__, url_prefix='/chart')
-    chart_blueprint.add_url_rule(rule='/')
 
+    dexcom: Dexcom = Dexcom(username=str(app_config.config["dexcom"]["username"]),
+        password=str(app_config.config["dexcom"]["password"]))
+
+    glucose_blueprint: Blueprint = Blueprint('glucose', import_name=__name__, url_prefix='/glucose')
+    def render_glucose_template(path: str) -> ft.ResponseReturnValue:
+        return app.send_static_file("glucose.html")
+    app.add_url_rule("/<path:path>", view_func=render_glucose_template)
+    app.add_url_rule("/", view_func=render_glucose_template, defaults={'path': ''})
     app.register_blueprint(blueprint=glucose_blueprint)
+
+    chart_blueprint: Blueprint = Blueprint('chart', import_name=__name__, url_prefix='/chart')
+    def render_chart_template(path: str) -> ft.ResponseReturnValue:
+        return app.send_static_file("chart.html")
+    app.add_url_rule("/<path:path>", view_func=render_chart_template)
+    app.add_url_rule("/", view_func=render_chart_template, defaults={'path': ''})
     app.register_blueprint(blueprint=chart_blueprint)
     return app
--- src/dexcom_browser_source/system_tray.py
+++ src/dexcom_browser_source/system_tray.py
@@ -4,7 +4,7 @@ from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QApplication, QMenu, QMessageBox, QSystemTrayIcon
 
 from dexcom_browser_source.about_dialog import AboutDialog
-from dexcom_browser_source.browser_source import BrowserSourceDetailsDialog, WaitressStatusDialog, WaitressThread
+from dexcom_browser_source.browser_source import BrowserSourceDetailsDialog
 from dexcom_browser_source.config import AppConfig
 from dexcom_browser_source.settings_dialog import SettingsDialog
 
@@ -14,39 +14,30 @@ class SystemTrayIcon(QSystemTrayIcon):
         if not self.isSystemTrayAvailable():
             _ = QMessageBox.critical(None, "Dexcom Browser Source", "The system tray is unavailable! Dexcom Browser Source will now close!")
             sys.exit(1)
-        super().__init__(parent)
-        self._waitress_thread: WaitressThread = WaitressThread(app_config)
 
-        self._waitress_status_dialog: WaitressStatusDialog = WaitressStatusDialog(parent=None, waitress_thread=self._waitress_thread)
-        self._browser_source_details_dialog: BrowserSourceDetailsDialog = BrowserSourceDetailsDialog(parent=None, waitress_thread=self._waitress_thread)
-        self._settings_dialog: SettingsDialog = SettingsDialog(parent=None, app_config=app_config)
-        self._about_dialog: AboutDialog = AboutDialog()
-
-        self._waitress_status_action: QAction = QAction()
+        self.browser_source_details_dialog: BrowserSourceDetailsDialog = BrowserSourceDetailsDialog(parent=None, app_config=app_config)
+        self.settings_dialog: SettingsDialog = SettingsDialog(parent=None, app_config=app_config)
+        self.about_dialog: AboutDialog = AboutDialog()
         self._browser_source_action: QAction = QAction()
         self._settings_action: QAction = QAction()
         self._about_action: QAction = QAction()
         self._quit_action: QAction = QAction()
-        self.create_actions()
+        super().__init__(parent)
+
         self._context_menu: QMenu = self.create_context_menu()
         self.setContextMenu(self._context_menu)
-        self._waitress_thread.start()
-
-    def create_actions(self):
-        self._waitress_status_action = QAction("Waitress Status", self)
-        _ = self._waitress_status_action.triggered.connect(self._waitress_status_dialog.show)
-        self._browser_source_action = QAction("Browser Source Details", self)
-        _ = self._browser_source_action.triggered.connect(self._browser_source_details_dialog.show)
-        self._settings_action = QAction("Settings", self)
-        _ = self._settings_action.triggered.connect(self._settings_dialog.show)
-        self._about_action = QAction("About", self)
-        _ = self._about_action.triggered.connect(self._about_dialog.show)
-        self._quit_action = QAction("Quit", self)
-        _ = self._quit_action.triggered.connect(QApplication.quit)
 
     def create_context_menu(self):
+        self._browser_source_action = QAction("Browser Source Details", self)
+        self._settings_action = QAction("Settings", self)
+        self._about_action = QAction("About", self)
+        self._quit_action = QAction("Quit", self)
+        _ = self._browser_source_action.triggered.connect(self.browser_source_details_dialog.show)
+        _ = self._settings_action.triggered.connect(self.settings_dialog.show)
+        _ = self._about_action.triggered.connect(self.about_dialog.show)
+        _ = self._quit_action.triggered.connect(QApplication.quit)
+
         context_menu = QMenu()
-        context_menu.addAction(self._waitress_status_action)
         context_menu.addAction(self._browser_source_action)
         context_menu.addAction(self._settings_action)
         context_menu.addAction(self._about_action)
