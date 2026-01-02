a7b1bdcbc555a58442645bf6129a4152ff69c695
possiblemeatball <meatball@manthrowshat.net>
2026-01-01 19:51:36 -0500

    show icon and app version string in about dialog

--- src/dexcom_browser_source/about_dialog.py
+++ src/dexcom_browser_source/about_dialog.py
@@ -1,9 +1,28 @@
-from PySide6.QtWidgets import QDialog, QWidget
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QDialog, QHBoxLayout, QLabel, QVBoxLayout, QWidget
+
+from dexcom_browser_source.config import AppConfig
 
 
 class AboutDialog(QDialog):
-    def __init__(self, parent: QWidget | None = None):
+    def __init__(self, app: QApplication, app_config: AppConfig, parent: QWidget | None = None):
+        self._app: QApplication = app
+        self._app_config: AppConfig = app_config
         super().__init__(parent)
-        self.resize(320, 200)
-        self.setFixedSize(320, 200)
+        self._layout: QVBoxLayout = QVBoxLayout()
+        self._app_label_layout: QHBoxLayout = QHBoxLayout()
+        self._app_icon: QIcon = QIcon("assets/icon.svg")
+        self._app_icon_label: QLabel = QLabel()
+        self._app_label: QLabel = QLabel(f"# {self._app.applicationDisplayName()} v{self._app.applicationVersion()}")
+
+        self._app_label.setTextFormat(Qt.TextFormat.MarkdownText)
+        self._app_icon_label.setPixmap(self._app_icon.pixmap(32))
+        self._app_label_layout.addWidget(self._app_icon_label)
+        self._app_label_layout.addWidget(self._app_label)
+        self._layout.addLayout(self._app_label_layout)
+
+        self.resize(self.minimumSize())
+        self.setFixedSize(self.minimumSize())
         self.setWindowTitle("About Dexcom Browser Source")
+        self.setLayout(self._layout)
--- src/dexcom_browser_source/system_tray.py
+++ src/dexcom_browser_source/system_tray.py
@@ -14,24 +14,22 @@ class SystemTrayIcon(QSystemTrayIcon):
         if not self.isSystemTrayAvailable():
             _ = QMessageBox.critical(None, "Dexcom Browser Source", "The system tray is unavailable! Dexcom Browser Source will now close!")
             sys.exit(1)
+
         self._app: QApplication = app
         self._app_config: AppConfig = app_config
-
         self._app_icon: QIcon = QIcon("assets/icon.svg")
         self.browser_source_details_dialog: BrowserSourceDetailsDialog = BrowserSourceDetailsDialog(parent=None, app=self._app, app_config=self._app_config)
         self.settings_dialog: SettingsDialog = SettingsDialog(parent=None, app=self._app, app_config=self._app_config)
-        self.about_dialog: AboutDialog = AboutDialog()
+        self.about_dialog: AboutDialog = AboutDialog(parent=None, app=self._app, app_config=self._app_config)
         self._browser_source_action: QAction = QAction()
         self._settings_action: QAction = QAction()
         self._about_action: QAction = QAction()
         self._quit_action: QAction = QAction()
         super().__init__(parent)
         self.setIcon(self._app_icon)
+        self.setContextMenu(self.create_context_menu())
 
-        self._context_menu: QMenu = self.create_context_menu()
-        self.setContextMenu(self._context_menu)
-
-    def create_context_menu(self):
+    def create_context_menu(self) -> QMenu:
         self._browser_source_action = QAction("Browser Source Details", self)
         self._settings_action = QAction("Settings", self)
         self._about_action = QAction("About", self)
@@ -39,9 +37,9 @@ class SystemTrayIcon(QSystemTrayIcon):
         _ = self._browser_source_action.triggered.connect(self.browser_source_details_dialog.show)
         _ = self._settings_action.triggered.connect(self.settings_dialog.show)
         _ = self._about_action.triggered.connect(self.about_dialog.show)
-        _ = self._quit_action.triggered.connect(QApplication.quit)
+        _ = self._quit_action.triggered.connect(self._app.quit)
 
-        context_menu = QMenu()
+        context_menu: QMenu = QMenu()
         context_menu.addAction(self._browser_source_action)
         context_menu.addAction(self._settings_action)
         context_menu.addAction(self._about_action)
